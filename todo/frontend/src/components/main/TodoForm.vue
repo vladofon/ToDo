3c3ec9befb8b952f6e7ad7e5ee5cd04379286d6d
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-10 19:14:36 +0300

    Create ToDo delete function

--- todo/frontend/src/components/main/TodoForm.vue
+++ todo/frontend/src/components/main/TodoForm.vue
@@ -3,7 +3,11 @@
 	
 		<v-row align="center" justify="center">
 			<v-col align="center" cols="6">
-				<v-text-field v-model="todo.text" @keyup.enter="createTodo"
+				<v-text-field 
+					v-model="todo.text" 
+					@keyup.enter="saveTodo"
+					@edit="editTodo"
+				
 					width="500px"
 					color="info"
 					label="ToDo"
@@ -23,19 +27,28 @@
 		data() {
 			return {
 				todo: {
+					id: null,
 					text: ''
 				}
 			}
 		},
 		methods: {
-			...mapMutations(['addTodo']),
-			createTodo() {
-				this.todo.id = Math.floor(Math.random() * 1000000)
-				this.addTodo(this.todo)
+			...mapMutations(['addTodo', 'updateTodo']),
+			saveTodo() {
+				if(this.todo.id) {
+					this.updateTodo(this.todo)
+				} else {
+					this.todo.id = Math.floor(Math.random() * 1000000)
+					this.addTodo(this.todo)
+				}
 				
 				this.todo = {
+					id: null,
 					text: ''
 				}
+			},
+			editTodo(todo) {
+				this.todo = todo
 			}
 		}
 	}
---
+++ todo/frontend/src/components/main/TodoMenu.vue
@@ -0,0 +1,38 @@
+<template>
+	<app-menu>
+		<template v-slot:action-buttons>
+		
+			<v-list-item>
+				<v-list-item-title>
+					<v-btn @click="removeTodo(todo.id)" class="text-red" plain>Delete</v-btn>
+				</v-list-item-title>
+			</v-list-item>
+			
+			<v-list-item>
+				<v-list-item-title>
+					<v-btn @click="$emit('edit', todo)" class="text-orange" plain>Edit</v-btn>
+				</v-list-item-title>
+			</v-list-item>
+			
+		</template>
+	</app-menu>
+</template>
+
+<script>
+	import AppMenu from '@/components/global/AppMenu.vue'
+	import { mapMutations } from 'vuex'
+	
+	export default {
+		components: {
+			AppMenu
+		},
+		props: ['todo'],
+		methods: {
+			...mapMutations([
+				'removeTodo',
+			]),
+			
+		}
+		
+	}
+</script>
