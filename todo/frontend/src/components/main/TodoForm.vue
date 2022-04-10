7d1bffcf96ad3bf74bd7ce98ae08d18925945e77
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-10 18:58:42 +0300

    Add ToDo creation function

--- todo/frontend/src/components/main/TodoForm.vue
+++ todo/frontend/src/components/main/TodoForm.vue
@@ -3,7 +3,8 @@
 	
 		<v-row align="center" justify="center">
 			<v-col align="center" cols="6">
-				<v-text-field  width="500px"
+				<v-text-field v-model="todo.text" @keyup.enter="createTodo"
+					width="500px"
 					color="info"
 					label="ToDo"
 					placeholder="Enter some task etc..."
@@ -14,3 +15,28 @@
 		
 	</v-container>
 </template>
+
+<script>
+	import { mapMutations } from 'vuex'
+	
+	export default {
+		data() {
+			return {
+				todo: {
+					text: ''
+				}
+			}
+		},
+		methods: {
+			...mapMutations(['addTodo']),
+			createTodo() {
+				this.todo.id = Math.floor(Math.random() * 1000000)
+				this.addTodo(this.todo)
+				
+				this.todo = {
+					text: ''
+				}
+			}
+		}
+	}
+</script>
