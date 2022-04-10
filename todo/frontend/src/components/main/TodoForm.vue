704d1545c4882b334da6abf408cdf5a118dc233a
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-10 15:50:36 +0300

    Create Form to enter new ToDo cards

---
+++ todo/frontend/src/components/main/TodoForm.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-container  align="center">
+	
+		<v-row align="center" justify="center">
+			<v-col align="center" cols="6">
+				<v-text-field  width="500px"
+					color="info"
+					label="ToDo"
+					placeholder="Enter some task etc..."
+					variant="contained">
+				</v-text-field>
+			</v-col>
+		</v-row>
+		
+	</v-container>
+</template>
