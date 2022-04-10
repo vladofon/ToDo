93448543446ad6dc76eb997fe904f166cef63613
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-10 19:15:51 +0300

    Connect ToDo list with store

--- todo/frontend/src/store/store.js
+++ todo/frontend/src/store/store.js
@@ -1,12 +1,30 @@
-export default {
+import { createStore } from 'vuex'
+
+export default createStore({
 	state: {
-		
+		todos: [
+			{id: 1, text: 'My current ToDo for this day'},
+			{id: 2, text: 'My current ToDo for this day'},
+			{id: 3, text: 'My current ToDo for this day'},
+			{id: 4, text: 'My current ToDo for this day'},
+			{id: 5, text: 'My current ToDo for this day'},
+		]
 	},
 	getters: {
 		
 	},
 	mutations: {
-		
+		addTodo(state, todo) {
+			state.todos.push(todo)
+		},
+		removeTodo(state, id) {
+			state.todos = state.todos.filter(item => item.id !== id)
+		},
+		updateTodo(state, todo) {
+			const index = state.todos.findIndex(item => item.id === todo.id)
+			
+			state.todos[index] = todo
+		}
 	},
 	actions: {
 		
@@ -14,4 +32,4 @@ export default {
 	modules: {
 		
 	}
-}
+})
