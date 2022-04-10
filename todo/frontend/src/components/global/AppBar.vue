e70e4c626e80be1a6909a749b5f8dc6d46e8a9b5
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-10 15:48:54 +0300

    Create AppBar component

---
+++ todo/frontend/src/components/global/AppBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar
+    color="info"
+    density="prominent"
+  >
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>ToDo list</v-app-bar-title>
+
+    <template v-slot:append>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </template>
+  </v-app-bar>
+</template>
