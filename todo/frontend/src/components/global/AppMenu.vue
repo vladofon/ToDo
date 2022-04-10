21360c476220b5f8a1cca18eaae0c64cffd38963
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-10 15:49:07 +0300

    Create AppMenu component

---
+++ todo/frontend/src/components/global/AppMenu.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-menu transition="scale-transition">
+		<template v-slot:activator="{ props }">
+			<v-btn v-bind="props" flat icon="mdi-dots-vertical" class="align-self-start"></v-btn>
+		</template>
+		
+		<v-list elevation="5" rounded class="pa-0">
+			<v-list-item>
+				<v-list-item-title>
+					<v-btn class="text-red" plain>Delete</v-btn>
+				</v-list-item-title>
+			</v-list-item>
+			<v-list-item>
+				<v-list-item-title><v-btn class="text-orange" plain>Edit</v-btn></v-list-item-title>
+			</v-list-item>
+		</v-list>
+	</v-menu>
+</template>
