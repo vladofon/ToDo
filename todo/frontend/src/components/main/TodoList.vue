ae437c46ba3146c322af18254ebddf92ab261e41
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-10 15:50:16 +0300

    Create List for ToDo cards

---
+++ todo/frontend/src/components/main/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-container class="">
+		<v-row>
+			<v-col align="center">
+				<todo-card v-for="card in 5" :key="card"/>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+	import TodoCard from '@/components/main/TodoCard.vue'
+	
+	export default {
+		components: {
+			TodoCard
+		}
+	}
+</script>
