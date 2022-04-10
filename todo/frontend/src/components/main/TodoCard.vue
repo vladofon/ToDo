fc54623a398eda57c71fdaf668b2577006838a90
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-10 15:49:56 +0300

    Create ToDo card styles

---
+++ todo/frontend/src/components/main/TodoCard.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-card class="my-2" max-width="500px">
+		<v-card-title class="d-flex justify-space-between">
+			<span>Current toDo</span>
+			<v-spacer></v-spacer>
+			
+			<app-menu>
+			
+			</app-menu>
+			
+		</v-card-title>
+	</v-card>
+</template>
+
+<script>
+	import AppMenu from '@/components/global/AppMenu.vue'
+	
+	export default {
+		components: {
+			AppMenu
+		}
+	}
+</script>
